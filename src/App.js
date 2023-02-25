196de88aed5af23a5592234affb42df2ec7aba80
aleksei-v <aleksey.vasilenko7@gmail.com>
2023-02-25 18:53:37 +0300

    update uri

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import SongDetail from './SongDetail';
 import { fetchCategories, fetchGenres, fetchPlaylist, fetchArtists } from './services/fretchMusic';
 function App() {
     const CLIENT_ID = 'dc433a935d2e4e958ad9d3f3caf9aca0'
-    const REDIRECT_URI = "https://aleksei-v.github.io/music-assortment/"
+    const REDIRECT_URI = "http://localhost:3000"
     const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
     const RESPONSE_TYPE = "token"
   
@@ -182,7 +182,7 @@ useEffect(() => {
           {artist.images.length
             ? <img width='100%' src={artist.images[0].url} alt={artist.name} />
             : <div>No image</div> }
-          {artist.name}
+           {artist.name}
         </div>
     ))}
 
