f464556df700e09f49779f5553395eb8def821f2
aleksei-v <95973001+aleksei-v@users.noreply.github.com>
2023-04-06 10:04:52 +0300

    Update StartPage.jsx

--- src/StartPage.jsx
+++ src/StartPage.jsx
@@ -3,8 +3,8 @@ import { Button } from 'react-bootstrap'
 
 const StartPage = () => {
     const CLIENT_ID = 'dc433a935d2e4e958ad9d3f3caf9aca0'
-    const REDIRECT_URI = "http://aleksei-v.github.io/music-assortment/"
-    // const REDIRECT_URI = "http://localhost:3000/music-assortment"
+    const REDIRECT_URI = "https://aleksei-v.github.io/music-assortment/"
+    // const REDIRECT_URI = "http://localhost:3000"
     const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
     const RESPONSE_TYPE = "token"
     return (
